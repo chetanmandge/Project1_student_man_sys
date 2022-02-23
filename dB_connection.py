f18fac9b29b8d74fdbd106fee7f337820c628e00
chetanmandge <chetanmandge05@gmail.com>
2022-02-23 21:20:42 +0530

    add student method is added

--- dB_connection.py
+++ dB_connection.py
@@ -1,1 +1,30 @@
 import mysql.connector
+
+# def db_connection():
+#     con = mysql.connector.connect(
+#         host="localhost" ,
+#         user="root" ,
+#         password="Chetan@05" ,
+#         auth_plugin='mysql_native_password'
+#     )
+#     print(con)
+
+def add_student():
+    con = mysql.connector.connect(
+        host="localhost" ,
+        user="root" ,
+        password="Chetan@05" ,
+        auth_plugin='mysql_native_password',
+        database = "students"
+    )
+    print(con)
+    cursor = con.cursor()
+    name = input("Enter Student Name: ")
+    marks = float(input("Enter Student Marks: "))
+    roll_no = int(input("Enter Student Roll Number: "))
+    query = f""" INSERT INTO student_info (name , marks , roll_no) 
+                        values('{name}' , {marks} , {roll_no}) """
+    cursor.execute(query)
+    con.commit()
+
+add_student()
